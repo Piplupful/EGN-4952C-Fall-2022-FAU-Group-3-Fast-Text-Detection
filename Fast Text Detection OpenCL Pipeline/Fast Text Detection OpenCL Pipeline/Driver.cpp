2a29cc78012bc9995c0f45a1e62367c0c2dd46b5
Leonardo Martinez <leos32111@gmail.com>
2022-11-25 09:37:07 -0500

    Command Line Input
    Title.
    
    4 Parameters:
    - Input File Path
    - Input File Name
    - Output File Path
    - Number of Runs

--- Fast Text Detection OpenCL Pipeline/Fast Text Detection OpenCL Pipeline/Driver.cpp
+++ Fast Text Detection OpenCL Pipeline/Fast Text Detection OpenCL Pipeline/Driver.cpp
@@ -7,7 +7,8 @@
 #include "ocv.h"
 using namespace std;
 
-int main()
+//SYS ARG FORMAT: Input Folder Path, Input File Name (with .yuv), Output File Path, Number of time to run Text Detection Driver
+int main(int argc, char* argv[])
 {
 	uint64_t width = 0;
 	uint64_t height = 0;
@@ -18,12 +19,24 @@ int main()
 
 	int numRuns;
 
-	//TEMPORARY UNTIL ARGV IMPLEMENTED
-	inputFilePath = "D:/xiph/50 Frame YUVs/";
-	yuvFileName = "GTAV_1920x1080p60_50Frame.yuv";
-	outputFilePath = "../OUTPUT/";
-	numRuns = 20;
-	//TEMP
+	if (argc >= 5)
+	{
+		cout << "USING PROVIDED VALUES" << endl;
+		inputFilePath = argv[1];
+		yuvFileName = argv[2];
+		outputFilePath = argv[3];
+		numRuns = atoi(argv[4]);
+		//Example Command Line Input: D:\xiph\50_Frame\ STARCRAFT_1920x1080p60_50Frame.yuv ../OUTPUT/ 5
+		//							Input File Path		YUV Input File Name		Output File Path	Number of Runs
+	}
+	else //Default values.
+	{
+		cout << "USING DEFAULT VALUES" << endl;
+		inputFilePath = "../../";
+		yuvFileName = "GTAV_1920x1080p60_50Frame.yuv";
+		outputFilePath = "../OUTPUT/";
+		numRuns = 1;
+	}
 
 	//Open YUV File
 	FILE* fp = openYUVFile(&width, &height, inputFilePath, yuvFileName);
@@ -36,6 +49,8 @@ int main()
 
 	//Initialize OpenCL Object, with .cl File Name, Kernel Function name, and Width of YUV Frame.
 	OpenCL ocl("model.cl", "kernelTemplateDebug", width);
+	//Width, for the purposes of this project, is set as Kernel Parameter 1.
+	//Within the Text Detection Driver, Parameter 0 is the frame buffer, and Parameter 2 is the binary map used for our output.
 
 	//Loop for multiple runs. Text detection driver function.
 	for (int i = 0; i < numRuns; i++)
